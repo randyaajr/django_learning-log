951171c5a9d7edc69563f952809747863eb9198b
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-01-24 17:47:33 -0500

    Added my app to installed apps

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'learning_logs'
+    #Defult django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
