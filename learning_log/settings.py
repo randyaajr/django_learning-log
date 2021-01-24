0af6860cb7f69a2219be856b49de4e365103a6b7
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-01-24 18:07:06 -0500

    Troublestooting issues when migrating. passed

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '15(=tbz^f%v@yfwrk$o_5i)wlhm@jhk8a^wl#*ptvhd6onrev('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # My apps
-    'learning_logs'
+    'learning_logs',
     #Defult django apps.
     'django.contrib.admin',
     'django.contrib.auth',
