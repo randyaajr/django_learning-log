e6e656679dcf848594797bcf9966f55d82cdcc2e
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-01-27 17:01:30 -0500

    Registered Entry with admin

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Entry
 
 # Register your models here.
 
 admin.site.register(Topic)
+admin.site.register(Entry)
