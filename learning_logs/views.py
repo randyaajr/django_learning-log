f0acc942fbbd16b5a53fa6520f1bb5c2cad90e41
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-01-27 20:16:16 -0500

    Added topics.html to the view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -6,3 +7,10 @@ from django.shortcuts import render
 def index(request):
     """ The Home page for learning_log. """
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """ Show all available topics """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
