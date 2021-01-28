b15cbab07f2268fd483eae8ce627a80a63d7f2a5
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-01-27 19:26:26 -0500

    View created for index.html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """ The Home page for learning_log. """
+    return render(request, 'learning_logs/index.html')
