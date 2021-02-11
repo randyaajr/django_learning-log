e5ecc54060486526bd20e1fa2e9cfd9e9f1cb72d
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-11 18:01:33 -0500

    Created view for edit entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -59,3 +59,22 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form.
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """ For editing an existing entry """
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
