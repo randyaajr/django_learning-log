c974c696b1c9110fbdd607d47cd7434bead5e9c4
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-11 16:16:42 -0500

    Form for entry created

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -7,3 +7,11 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
