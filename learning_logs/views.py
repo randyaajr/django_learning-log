24155b884e15fefb36a9c428b62bad295584bccd
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-11 14:55:37 -0500

    Imported redirect & TopicForm. Created view for new_topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
-
-# Create your views here.
+from .forms import TopicForm
 
 
 def index(request):
@@ -22,3 +21,20 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """ Add new topic """
+    if request.method != 'POST':
+        # No data submitted; create blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return redirect(request, 'learning_logs/new_topic.html', context)
