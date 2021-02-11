02a37831646ebb23ab3a57d493de77fd00999b43
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-11 16:34:04 -0500

    View created for new entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -38,3 +38,23 @@ def new_topic(request):
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """ Add a new entry for a particular topic """
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; create blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
