d9e6139f0094ed1536e1c61d20ce649ebb155c73
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-11 16:18:50 -0500

    Path added for new entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     # This page adds the new topic
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
