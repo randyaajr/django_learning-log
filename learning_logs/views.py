6efff80f83bd34943d3e72bf52d2c2c7fd32799a
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-12 18:40:09 -0500

    Owner of topic must match current logged in user or 404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -77,6 +77,8 @@ def edit_entry(request, entry_id):
     """ For editing an existing entry """
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
