c24028854003fed6ac635fa9048158cd5402fb07
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-12 18:56:54 -0500

    Fixed bug preventing user from adding new topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -42,7 +42,9 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Display a blank or invalid form.
