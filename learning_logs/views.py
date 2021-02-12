67e191c6c904a5ef3b8bde3c27af486a335d4bfd
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-12 17:47:48 -0500

    imported login_required from django contrib auth decorators

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
