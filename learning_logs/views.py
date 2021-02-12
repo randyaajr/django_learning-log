7320ca4961a37830121e34a846072c895a607a1b
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-12 17:58:52 -0500

    Restrictions added for users not logged in

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -18,6 +18,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """ Show a single topic and all its entries """
     topic = Topic.objects.get(id=topic_id)
@@ -26,6 +27,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """ Add new topic """
     if request.method != 'POST':
@@ -43,6 +45,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """ Add a new entry for a particular topic """
     topic = Topic.objects.get(id=topic_id)
@@ -64,6 +67,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """ For editing an existing entry """
     entry = Entry.objects.get(id=entry_id)
