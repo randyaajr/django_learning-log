006cf54034bd097bfba374d123deb76047965b88
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-12 18:38:13 -0500

    Topic user must match current user or 404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -23,6 +23,10 @@ def topics(request):
 def topic(request, topic_id):
     """ Show a single topic and all its entries """
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
