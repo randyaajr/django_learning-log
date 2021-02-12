0fb1f8dfeaf478138347946ba879a91edd1e0aad
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-02-12 17:50:07 -0500

    Each topic owned by a user, so only registered user can request topics page..

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,6 +10,7 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     """ Show all available topics """
     topics = Topic.objects.order_by('date_added')
